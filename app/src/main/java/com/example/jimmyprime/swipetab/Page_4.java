546d2c49ab3d8750bb412e5194055250f5e9cd70
Jimmy Lee <b00901058@ntu.edu.tw>
2014-12-08 16:46:04 +0800

    Initial Commit
    4 tabbar
    swipe disable

---
+++ app/src/main/java/com/example/jimmyprime/swipetab/Page_4.java
@@ -0,0 +1,21 @@
+package com.example.jimmyprime.swipetab;
+
+/**
+ * Created by JimmyPrime on 2014/10/26.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Page_4 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View page_4 = inflater.inflate(R.layout.page_4_frag, container, false);
+        // ((TextView)page_4.findViewById(R.id.textView)).setText("Page 4");
+        return page_4;
+    }
+}
